abc85f5e2e6fb6f7c1b3c1c86aa35d6f3935f99f
Josh Grainge <josh.grainge@hotmail.com>
2023-03-07 13:34:14 -0500

    Added project skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [cards, setCards] = useState();
+
+  function cardClick(id) {
+    // If card clicked, gameover
+    // Else increase score
+    // If all cards have been clicked you win the round
+  }
+
+  // Map over card data and create card components for each element
+
+  return <div></div>;
 }
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,6 @@
+function Card() {
+  // Set card image & title to be value in property
+  // Set onclick handler to be props handleClick
+}
+
+export default Card;
